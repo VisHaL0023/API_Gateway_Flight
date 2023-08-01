3d681f4830d9d83e236eb15860b1821bd79ba5f9
VisHaL0023 <vishalpanchal1828vp@gmail.com>
2023-08-02 02:33:59 +0530

    added signin with JWT token

--- src/routes/v1/user-routes.js
+++ src/routes/v1/user-routes.js
@@ -4,4 +4,5 @@ const { AuthRequestMiddlewares } = require('../../middlewares');
 const router = express.Router();
 
 router.post('/signup', AuthRequestMiddlewares.validateAuthRequest, UserController.signup);
+router.post('/signin',AuthRequestMiddlewares.validateAuthRequest, UserController.signin);
 module.exports = router;
